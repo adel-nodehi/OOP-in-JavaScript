a56502216fcb644b373b4456610eb902dc380ccd
Adel <adelnmo2003@gmail.com>
2024-04-28 16:13:06 +0330

    Inheritance Between Classes, Object.create

--- script.js
+++ script.js
@@ -235,6 +235,7 @@ console.dir(Student.prototype.constructor);
 console.log(mike);
 */
 
+/*
 ///////////////////////////////////////
 // Inheritance Between "Classes": ES6 Classes
 
@@ -296,3 +297,36 @@ const martha = new StudentCl('Martha jones', 2012, 'Computer Science');
 martha.introduce();
 martha.calcAge();
 console.log(martha);
+*/
+
+///////////////////////////////////////
+// Inheritance Between "Classes": Object.create
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I'm study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+jay.introduce();
+jay.calcAge();
