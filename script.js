df079154e48deeef62decd65a5f4f236206aece0
Adel <adelnmo2003@gmail.com>
2024-04-28 09:35:22 +0330

    14-15 inheritance between classes, constructor function

--- script.js
+++ script.js
@@ -165,6 +165,7 @@ PersonCl.hey();
 // jessica.hey();
 */
 
+/*
 ///////////////////////////////////////
 // Object.create
 
@@ -190,3 +191,44 @@ console.log(stiven.__proto__ === PersonProto);
 const sara = Object.create(PersonProto);
 sara.init('Sara', 1992);
 sara.calcAge();
+*/
+
+///////////////////////////////////////
+// Inheritance Between "Classes": Constructor Functions
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+// Add before write any method
+Student.prototype = Object.create(Person.prototype);
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I'm study ${this.course}`);
+};
+
+const mike = new Student('Mike', 2020, 'Computer Science');
+
+mike.introduce();
+mike.calcAge();
+
+console.log(mike.__proto__);
+console.log(mike.__proto__.__proto__);
+
+console.log(mike instanceof Student);
+console.log(mike instanceof Person);
+console.log(mike instanceof Object);
+
+Student.prototype.constructor = Student;
+console.dir(Student.prototype.constructor);
+console.log(mike);
