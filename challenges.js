ce20899d816858ba3d8e74109e52bad7cedf806b
Adel <adelnmo2003@gmail.com>
2024-04-28 17:57:04 +0330

    Challenge 14 #4

--- challenges.js
+++ challenges.js
@@ -798,7 +798,7 @@ ford.speedUS = 50;
 console.log(ford);
 */
 
-// 14 CHALLENGE #3
+/* // 14 CHALLENGE #3
 
 const car = function (make, speed) {
   this.make = make;
@@ -843,3 +843,74 @@ tesla.chargeBattery(90);
 tesla.accelerate();
 
 console.log(tesla);
+*/
+
+// 14 CHALLENGE #4
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+
+    return this;
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+class EVCl extends CarCl {
+  #charge;
+
+  constructor(make, speed, charge) {
+    super(make, speed);
+
+    this.#charge = charge;
+  }
+
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+
+    return this;
+  }
+
+  accelerate() {
+    this.speed += 20;
+    this.#charge--;
+    console.log(
+      `${this.make} is going at ${this.speed} km/h, with a charge of ${
+        this.#charge
+      }`
+    );
+
+    return this;
+  }
+}
+
+const rivian = new EVCl('Rivian', 120, 23);
+
+rivian
+  .accelerate()
+  .accelerate()
+  .accelerate()
+  .brake()
+  .chargeBattery(50)
+  .accelerate();
+
+console.log(rivian);
+console.log(rivian.speedUS);
