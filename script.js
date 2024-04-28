04784b28e1bf7d79af82db323bc1fa493543f56c
Adel <adelnmo2003@gmail.com>
2024-04-28 17:27:31 +0330

    14-22 Chaning Methods

--- script.js
+++ script.js
@@ -371,10 +371,14 @@ class Account {
 
   deposit(val) {
     acc1.#movements.push(val);
+
+    return this;
   }
 
   withdraw(val) {
     this.deposit(-val);
+
+    return this;
   }
 
   requestLoan(val) {
@@ -382,6 +386,8 @@ class Account {
       this.deposit(val);
       console.log('Loan approved');
     }
+
+    return this;
   }
 
   static helper() {
@@ -402,10 +408,12 @@ const acc1 = new Account('Jonas', 'EUR', 1111);
 acc1.deposit(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
-
 console.log(acc1);
-// console.log(acc1.pin);
+Account.helper();
 
+// console.log(acc1.pin);
 // console.log(acc1.#movements);
 
-Account.helper();
+// Chaining
+acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
+console.log(acc1.getMovements());
